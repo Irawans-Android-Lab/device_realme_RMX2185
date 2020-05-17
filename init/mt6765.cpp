51002fc3f7720107482d1415a8556e23402af89c
cyberwalkman <firefox883@gmail.com>
2020-05-17 20:27:20 +0500

    RMX2185: Set dalvik-heap configuration dynamically
     * We have variants with 2, 3 and 4GB ram.
    
    Change-Id: I5eef1e092e54444c73bd3f92bba7291a54321253
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

---
+++ init/mt6765.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2020 LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sys/sysinfo.h>
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include <android-base/properties.h>
+#include <android-base/logging.h>
+
+#include "vendor_init.h"
+
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    prop_info *pi;
+    pi = (prop_info *)__system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void set_dalvik()
+{
+    // Set dalvik heap configuration
+    char const *heapstartsize;
+    char const *heapgrowthlimit;
+    char const *heapsize;
+    char const *heapminfree;
+    char const *heapmaxfree;
+    char const *heaptargetutilization;
+
+    struct sysinfo sys;
+    sysinfo(&sys);
+
+    if (sys.totalram > 3072ull * 1024 * 1024) {
+        // from - phone-xxhdpi-4096-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    } else {
+        // from - phone-xhdpi-2048-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.75";
+        heapminfree = "512k";
+        heapmaxfree = "8m";
+    }
+
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+}
+
+void vendor_load_properties()
+{
+    set_dalvik();
+}
