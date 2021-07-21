bb58474ac272a31e55f750bcc0a8630f3cbe2de5
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-07-21 01:37:43 +0000

    RMX2185: init: Set device model, build fingerprint and description via init
    * Refs
    https://github.com/dev-harsh1998/android_device_realme_X2/blob/lineage-18.1/init/x2.cpp
    https://github.com/Thagoo/android_device_xiaomi_rova/blob/lineage-18.1/init/init_rova.cpp
    
    Co-authored-by: Harshit Jain <god@hyper-labs.tech>
    Co-authored-by: Thagoo <lohitgowda56@gmail.com>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- init/mt6765.cpp
+++ init/mt6765.cpp
@@ -18,6 +18,23 @@
 
 #include "vendor_init.h"
 
+struct RMX218X_props
+{
+    std::string build_description;
+    std::string build_fingerprint;
+    std::string device_build;
+    std::string product_device;
+};
+
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "vendor.",
+    "system_ext.",
+};
+
 void property_override(char const prop[], char const value[], bool add = true)
 {
     prop_info *pi;
@@ -26,6 +43,68 @@ void property_override(char const prop[], char const value[], bool add = true)
         __system_property_update(pi, value, strlen(value));
     else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void setRMX(unsigned int variant)
+{
+    RMX218X_props prop[4] = {};
+
+    std::string build_desc = "full_oppo6765-user 10 QP1A.190711.020 bedd37e98646d3a1 release-keys";
+    std::string build_fingerprint = "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys";
+
+    // RMX2180
+    prop[0] = {
+        build_desc,
+        build_fingerprint,
+        "RMX2180",
+        "RMX2180",
+    };
+
+    // RMX2189
+    prop[1] = {
+        build_desc,
+        build_fingerprint,
+        "RMX2189",
+        "RMX2189",
+    };
+
+    // RMX2185
+    prop[2] = {
+        build_desc,
+        build_fingerprint,
+        "RMX2185",
+        "RMX2185",
+    };
+
+    const auto set_ro_build_prop = [](const std::string &source,
+                                      const std::string &prop, const std::string &value) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    const auto set_ro_product_prop = [](const std::string &source,
+                                        const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    property_override("ro.build.description", prop[variant].build_description.c_str());
+    property_override("ro.build.product", prop[variant].product_device.c_str());
+    for (const auto &source : ro_props_default_source_order)
+    {
+        set_ro_build_prop(source, "fingerprint", prop[variant].build_fingerprint.c_str());
+        set_ro_product_prop(source, "device", prop[variant].product_device.c_str());
+        set_ro_product_prop(source, "model", prop[variant].device_build.c_str());
+    }
+    property_override("ro.build.fingerprint", prop[variant].build_fingerprint.c_str());
+    property_override("ro.bootimage.build.fingerprint", prop[variant].build_fingerprint.c_str());
+    property_override("ro.product.bootimage.name", prop[variant].device_build.c_str());
+    property_override("ro.odm.build.fingerprint", prop[variant].build_fingerprint.c_str());
+    property_override("ro.vendor.build.fingerprint", prop[variant].build_fingerprint.c_str());
+    property_override("ro.vendor.oppo.fingerprint", prop[variant].build_fingerprint.c_str());
+    property_override("ro.product.odm.name", prop[variant].device_build.c_str());
+    property_override("ro.product.vendor.name", prop[variant].device_build.c_str());
+    property_override("ro.vendor.oppo.product.name", prop[variant].device_build.c_str());
 }
 
 void set_dalvik()
@@ -69,5 +148,21 @@ void set_dalvik()
 
 void vendor_load_properties()
 {
+    std::ifstream fin;
+    std::string buf;
+    fin.open("/proc/cmdline");
+    while (std::getline(fin, buf, ' '))
+      if (buf.find("board_id") != std::string::npos)
+          break;
+    fin.close();
+
+    if (buf.find("S98635AA1") != std::string::npos) {
+        setRMX(0); // RMX2180
+    } else if (buf.find("S98639AA1") != std::string::npos) {
+        setRMX(1); //RMX2189
+    } else {
+        setRMX(2); // RMX2185
+    }
+
     set_dalvik();
 }
