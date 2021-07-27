31a1c545f7440e805647dfc9c5b33b2c366c761a
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-07-27 02:23:59 +0000

    RMX2185: rootdir: Update zram scripts based on available ram
    * Picked from stock rom
    https://github.com/MinatiScape/realme_rmx2185_dump/blob/RMX2185_11.A.93_0930_202104192215/system/system/etc/swap_enable.sh

--- rootdir/bin/set_zram.sh
+++ rootdir/bin/set_zram.sh
@@ -1,15 +1,23 @@
 #! /vendor/bin/sh
 
-ZMEM=$(cat /proc/meminfo | grep MemTotal | awk  '{print $2}')
-
-# Setup ZRAM to 50% of memtotal
-let 'ZMEM=((ZMEM/100)*50)*1024'
+# Setup ZRAM Based On Available Ram
+MemTotalStr=`cat /proc/meminfo | grep MemTotal`
+MemTotal=${MemTotalStr:16:8}
 echo 1 > /sys/block/zram0/reset
 echo 'zstd' > /sys/block/zram0/comp_algorithm
 sleep 0.5
-echo $ZMEM > /sys/block/zram0/disksize
+if [ $MemTotal -le 3145728 ]; then
+  #config 1.6GB zram size with memory less or equals to 3 GB
+  echo 1717986918 > /sys/block/zram0/disksize
+  echo 180 > /proc/sys/vm/swappiness
+  echo 0 > /proc/sys/vm/direct_swappiness
+else
+  #config 2GB zram size with memory less or equals to 4 GB
+  echo 2147483648 > /sys/block/zram0/disksize
+  echo 160 > /proc/sys/vm/swappiness
+  echo 60 > /proc/sys/vm/direct_swappiness
+fi
 sleep 0.5
 mkswap /dev/block/zram0
 sleep 0.5
 swapon -d /dev/block/zram0
-echo 100 > /proc/sys/vm/swappiness
